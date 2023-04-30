1ef6f581a5e835f7b9906c8c6ee3413e2bb8307d
Kelly Schuster Paredes <kellyschusterparedes@Kellys-MacBook-Pro-3.local>
2023-04-30 08:35:54 -0400

    files cleanup

--- app.py
+++ app.py
@@ -1,84 +1,110 @@
-from flask import Flask, render_template, redirect, jsonify
-from unesco_data import data, schools
-
-import plotly.graph_objs as go
-import json
-from pymongo import MongoClient
-import folium
+from dash import Dash, dcc, html, Input, Output
+import plotly.express as px
+from data.student_demographics import demographics
 import pandas as pd
 
-app = Flask(__name__)
+#Start a dash instance
+app = Dash(__name__)
 
-# Connect to MongoDB
-# client = MongoClient('mongodb://localhost:27017/')
-# db = client['students']
-# collection = db['student_data']
+colors = {
+    'background': '#ffffff',
+    'text': '#000000'
+}
 
-@app.route('/')
-def index():
-    # Query student data from MongoDB
-    # students = {}
-    # for doc in collection.find():
-    #     student = {
-    #         'age': doc['age'],
-    #         'siblings': doc['siblings'],
-    #         'interests': doc['interests'],
-    #         'gpa': doc['gpa']
-    #     }
+demographics_df = pd.DataFrame(demographics)
+# Create a bar graph of student GPAs
+graph_data = px.bar(demographics_df, x='Name', y='GPA',
+                    template='plotly_dark',
+                    labels={
+                        'GPA': 'Student GPA'})
 
-    #USE FAKE DATABASE FOR NOW
-    # Replace the MongoDB connection code with a dictionary
-    students = {
-        'Jane Doe': {'age': 13, 'siblings': 3, 'interests': 'dance', 'gpa': 3.7},
-        'John Smith': {'age': 15, 'siblings': 1, 'interests': 'soccer', 'gpa': 3.5},
-        'Samantha Johnson': {'age': 14, 'siblings': 2, 'interests': 'music', 'gpa': 4.0},
-        'Adam Lee': {'age': 13, 'siblings': 0, 'interests': 'science', 'gpa': 3.2},
-        'Emily Kim': {'age': 15, 'siblings': 2, 'interests': 'art', 'gpa': 3.9}
+#make the colors different for the graph
+graph_data.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+#add a scatterplot table √'Hours on Social Media': 1, 'Distance to School (miles)': 3
+fig = px.scatter(demographics_df, x="Age", y="Distance to School (miles)",
+                 size="Hours on Social Media", color="Grade", hover_name="Name",
+                 log_x=True, size_max=60)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+])
+
+
+# Create a dropdown menu of student names
+dropdown_menu = dcc.Dropdown(
+    id='student-dropdown',
+    options=[{'label': student['Name'], 'value': student['Name']} for student in demographics],
+    value=demographics_df['Name'].iloc[0]
+)
+
+# Create a student demographic table
+def create_demographic_table(student_name):
+      # Find the row in the dataframe that matches the selected student name
+    student_row = demographics_df.loc[demographics_df['Name'] == student_name]
+    
+    # If the row is not found, return an empty list
+    if student_row.empty:
+        return []
+    
+    # Create a table of the demographics of a student
+    return html.Table([
+        html.Tr([html.Td('Name:'), html.Td(student_row['Name'].iloc[0])]),
+        html.Tr([html.Td('Age:'), html.Td(str(student_row['Age'].iloc[0]))]),
+        html.Tr([html.Td('Grade:'), html.Td(str(student_row['Grade'].iloc[0]))]),
+        html.Tr([html.Td('DOB:'), html.Td(student_row['DOB'].iloc[0])]),
+        html.Tr([html.Td('Siblings:'), html.Td(str(student_row['Siblings'].iloc[0]))]),
+        html.Tr([html.Td('Hours on Social Media:'), html.Td(str(student_row['Hours on Social Media'].iloc[0]))]),
+        html.Tr([html.Td('Distance to School (miles):'), html.Td(str(student_row['Distance to School (miles)'].iloc[0]))]),
+        html.Tr([html.Td('Athletic:'), html.Td(str(student_row['Athletic'].iloc[0]))]),
+        html.Tr([html.Td('In the Band:'), html.Td(str(student_row['In the Band'].iloc[0]))]),
+        html.Tr([html.Td('GPA:'), html.Td(str(student_row['GPA'].iloc[0]))])
+    ],
+        style={'color': 'black'})
+
+
+# DFix the layout of the app
+app.layout = html.Div([
+    html.H1(
+        children='Student Demographics', 
+        style={'backgroundColor': colors['background'],
+        'color': colors['text']}),
+    
+    #dash interactive visualizations
+    dcc.Graph(
+        id='gpa-bar-chart',
+        figure=graph_data,
+        style={
+        'backgroundColor': colors['background'],
+        'color': '#333333'
     }
+    ),
 
-    # students[doc['name']] = student
-
-    # Create a bar graph of student GPAs
-    graph_data = [go.Bar(
-                x=list(students.keys()),
-                y=[student['gpa'] for student in students.values()]
-            ).to_plotly_json()]
-
-    # Create a dropdown menu of student names
-    dropdown_menu = [{'label': name, 'value': name} for name in students.keys()]
-
-    # Render the template with the graph and dropdown menu
-    return render_template('index.html', graph_data=json.dumps(graph_data), dropdown_menu=dropdown_menu)
-
-@app.route('/schools_map')
-def schools_map():
-    # Get data and create map
-    data = data
-    # map_html = get_map(schools_data)
-    # Create a map of schools
-    map_data = {
-            'type': 'scattergeo',
-        'locationmode': 'ISO-3',
-        'lat': data['lat'],
-        'lon': data['lon'],
-        'text': data['name'],
-        'mode': 'markers',
-        'marker': {
-            'color': data['color'],
-            'size': data['size'],
-            'opacity': 0.7,
-            'colorscale': 'Viridis',
-            'reversescale': True,
-            'colorbar': {'title': 'Number of Students'}
-        },
-    }
-
-    layout = {'title': 'UNESCO Schools Map'}
-
-    # Render the template with the map and dropdown menu
-    return render_template('schools_map.html', map_data=map_data, layout=layout, dropdown_menu=request.args.get('dropdown_menu'))
+    html.Div([
+        html.H2('Student Demographic Information'),
+        dropdown_menu,
+        html.Div(id='demographic-table')
+    ],
+    style={'backgroundColor': colors['background']})
+],
+    style={'backgroundColor': colors['background']}
+)
 
 
+# Define the callback function to update the student demographic table
+@app.callback(Output('demographic-table', 'children'),
+              Input('student-dropdown', 'value'))
+def update_demographic_table(student_name):
+    return create_demographic_table(student_name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    '''Dash includes "hot-reloading" 
+    Dash will automatically refresh your browser when you make a change in your code.'''
+    app.run_server(debug=True)
--- data/student_demographics.py
+++ data/student_demographics.py
@@ -7,7 +7,7 @@ demographics = [
     {'Name': 'Lily Nguyen', 'Age': 7, 'Grade': 2, 'DOB': 'November 7th, 2015', 'Siblings': 1, 'Hours on Social Media': 1.5, 'Distance to School (miles)': 4, 'Athletic': False, 'In the Band': True, 'GPA': 3.7},
     {'Name': 'William Kim', 'Age': 8, 'Grade': 3, 'DOB': 'January 18th, 2014', 'Siblings': 0, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 1, 'Athletic': True, 'In the Band': False, 'GPA': 3.3},
     {'Name': 'Chloe Davis', 'Age': 6, 'Grade': 1, 'DOB': 'August 12th, 2015', 'Siblings': 2, 'Hours on Social Media': 1, 'Distance to School (miles)': 2, 'Athletic': False, 'In the Band': True, 'GPA': 3.9},
-    {'Name': 'James Garcia', 'Age': 9, 'Grade': 4, 'DOB': 'May 14th, 2013', 'Siblings': 3, 'Hours on Social Media': 1, 'Distance to School (miles)': 3, 'Athletic': True, },
+    {'Name': 'James Garcia', 'Age': 9, 'Grade': 4, 'DOB': 'May 14th, 2013', 'Siblings': 3, 'Hours on Social Media': 1, 'Distance to School (miles)': 3, 'Athletic': True, 'In the Band': True, 'GPA': 2.9} ,
     {'Name': 'John Smith', 'Age': 15, 'Grade': 10, 'DOB': 'March 5th, 2008', 'Siblings': 1, 'Hours on Social Media': 2.5, 'Distance to School (miles)': 5, 'Athletic': False, 'In the Band': True, 'GPA': 3.6},
     {'Name': 'Sophia Lee', 'Age': 17, 'Grade': 12, 'DOB': 'October 22nd, 2005', 'Siblings': 3, 'Hours on Social Media': 1, 'Distance to School (miles)': 12, 'Athletic': True, 'In the Band': False, 'GPA': 3.9},
     {'Name': 'Daniel Kim', 'Age': 16, 'Grade': 11, 'DOB': 'June 17th, 2006', 'Siblings': 0, 'Hours on Social Media': 3, 'Distance to School (miles)': 3, 'Athletic': True, 'In the Band': False, 'GPA': 4.0},
