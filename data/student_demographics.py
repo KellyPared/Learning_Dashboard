53893477f681213876b8967090ba574bed6ee059
Kelly Schuster Paredes <kellyschusterparedes@Kellys-MacBook-Pro-3.local>
2023-04-30 07:04:34 -0400

    files cleanup

---
+++ data/student_demographics.py
@@ -0,0 +1,16 @@
+demographics = [
+    {'Name': 'Oliver Brown', 'Age': 9, 'Grade': 4, 'DOB': 'June 2nd, 2014', 'Siblings': 1, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 2, 'Athletic': True, 'In the Band': False, 'GPA': 3.5},
+    {'Name': 'Sophie Davis', 'Age': 7, 'Grade': 2, 'DOB': 'December 3rd, 2015', 'Siblings': 2, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 1, 'Athletic': False, 'In the Band': True, 'GPA': 3.8},
+    {'Name': 'Ethan Lee', 'Age': 8, 'Grade': 3, 'DOB': 'September 20th, 2014', 'Siblings': 0, 'Hours on Social Media': 1, 'Distance to School (miles)': 5, 'Athletic': True, 'In the Band': False, 'GPA': 3.2},
+    {'Name': 'Ava Rodriguez', 'Age': 6, 'Grade': 1, 'DOB': 'April 10th, 2016', 'Siblings': 3, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 3, 'Athletic': False, 'In the Band': True, 'GPA': 3.9},
+    {'Name': 'Noah Hernandez', 'Age': 9, 'Grade': 4, 'DOB': 'July 25th, 2013', 'Siblings': 2, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 1, 'Athletic': True, 'In the Band': False, 'GPA': 3.6},
+    {'Name': 'Lily Nguyen', 'Age': 7, 'Grade': 2, 'DOB': 'November 7th, 2015', 'Siblings': 1, 'Hours on Social Media': 1.5, 'Distance to School (miles)': 4, 'Athletic': False, 'In the Band': True, 'GPA': 3.7},
+    {'Name': 'William Kim', 'Age': 8, 'Grade': 3, 'DOB': 'January 18th, 2014', 'Siblings': 0, 'Hours on Social Media': 0.5, 'Distance to School (miles)': 1, 'Athletic': True, 'In the Band': False, 'GPA': 3.3},
+    {'Name': 'Chloe Davis', 'Age': 6, 'Grade': 1, 'DOB': 'August 12th, 2015', 'Siblings': 2, 'Hours on Social Media': 1, 'Distance to School (miles)': 2, 'Athletic': False, 'In the Band': True, 'GPA': 3.9},
+    {'Name': 'James Garcia', 'Age': 9, 'Grade': 4, 'DOB': 'May 14th, 2013', 'Siblings': 3, 'Hours on Social Media': 1, 'Distance to School (miles)': 3, 'Athletic': True, },
+    {'Name': 'John Smith', 'Age': 15, 'Grade': 10, 'DOB': 'March 5th, 2008', 'Siblings': 1, 'Hours on Social Media': 2.5, 'Distance to School (miles)': 5, 'Athletic': False, 'In the Band': True, 'GPA': 3.6},
+    {'Name': 'Sophia Lee', 'Age': 17, 'Grade': 12, 'DOB': 'October 22nd, 2005', 'Siblings': 3, 'Hours on Social Media': 1, 'Distance to School (miles)': 12, 'Athletic': True, 'In the Band': False, 'GPA': 3.9},
+    {'Name': 'Daniel Kim', 'Age': 16, 'Grade': 11, 'DOB': 'June 17th, 2006', 'Siblings': 0, 'Hours on Social Media': 3, 'Distance to School (miles)': 3, 'Athletic': True, 'In the Band': False, 'GPA': 4.0},
+    {'Name': 'Emily Chen', 'Age': 15, 'Grade': 10, 'DOB': 'December 1st, 2008', 'Siblings': 2, 'Hours on Social Media': 1.5, 'Distance to School (miles)': 10, 'Athletic': False, 'In the Band': True, 'GPA': 3.7},
+    {'Name': 'Jacob Park', 'Age': 18, 'Grade': 12, 'DOB': 'August 4th, 2004', 'Siblings': 4, 'Hours on Social Media': 2, 'Distance to School (miles)': 15, 'Athletic': True, 'In the Band': False, 'GPA': 3.8},
+]
